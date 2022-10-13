5ae18ae9d29338524b59e98c3e7e259e635698b2
Minutu <113061404+Minutu@users.noreply.github.com>
2022-10-13 14:13:49 -0400

    solved

---
+++ dngle.py
@@ -0,0 +1,2 @@
+ma_liste=['a','b','c','']
+print(zip(ma_liste, range (len(ma_liste))))
--- exercice.py
+++ exercice.py
@@ -6,26 +6,45 @@ from matplotlib.colors import cnames
 
 def list_to_dict(some_list: list) -> dict:
     # TODO: Transformer la liste en dictionnaire, les éléments de la liste deviennent les clés et leur index deviennent les valeurs
-
-    return {}
+    dict={}
+    for value, index in enumerate(some_list):
+        dict[index]=value
+    return dict
 
 
 def color_name_to_hex(colors: list) -> list:
     # TODO: Trouver la valeur hex de chaque couleur dans la liste et créer une liste de tupple où le premier élément est le nom de la couleur et le deuxième est la valeur hex
-
-    return []
+    my_list=[]
+    for i in colors:
+        my_list.append((i,cnames[i]))
+    return my_list
 
 
 def create_list() -> list:
     # TODO: Créer une liste des 10 000 premiers entiers positif, sauf pour les entiers de 15 à 350
+    number=0
+    liste=[]
+    while number<10000:
+        if number<15:
+            liste.append(number)
+            number+=1
+        elif number >=15 and number<350:
+            number+=1
+        elif number>=350 and number<10000:
+            liste.append(number)
+            number+=1
 
-    return []
+    return liste
 
 
 def compute_mse(model_dict: dict) -> dict:
     # TODO: Calculer l'erreur quadratique moyen pour chaque modèle. Retourner un dictionnaire contenant les MSE.
-
-    return {}
+    for model, results in model_dict.items():
+        error=0
+        for result in results:
+            error+=(result[1]-result[0])**2
+        model_dict[model]=error/len(results)
+    return model_dict
 
 
 def main() -> None:
